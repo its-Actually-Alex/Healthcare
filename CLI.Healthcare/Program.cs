5571009a740ac696e9418183e28fb8b3fe1c52d9
alexk <alexk@LAPTOP-NH8J4SQN>
2025-09-03 10:08:21 -0400

    Add project files.

---
+++ CLI.Healthcare/Program.cs
@@ -0,0 +1,52 @@
+﻿using Library.Healthcare.Models;
+using System;
+
+namespace CLI.Healthcare
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the healthcare thingy!");
+
+            List<Patient?> patients = new List<Patient?>();
+
+            do
+            {
+                Console.WriteLine("C. Create a New Patient Account");
+                Console.WriteLine("V. View Patients");
+                Console.WriteLine("D. Delete a Patient Account");
+                Console.WriteLine("Q. Quit");
+
+                var userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "C":
+                    case "c":
+                        var patient = new Patient();
+                        patient.Name = Console.ReadLine();
+                        patient.Address = Console.ReadLine();
+                        patients.Add(patient);
+                        break;
+                    case "R":
+                    case "r":
+                        foreach(var p in patients)
+                        {
+                            Console.WriteLine($"({p?.Length}) {p}");
+                        }
+                        break;
+                    case "D":
+                    case "d":
+                        break;
+                    case "Q":
+                    case "q":
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Command!");
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Library.Healthcare/Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Healthcare.Models
+{
+    public class Patient
+    {
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Birthdate { get; set; }
+        public char? Gender { get; set; }
+        public int? Age { get; set; }
+        public int Id { get; set; }
+    }
+}
